900ddd3fd83e556aa730464041ffa6ce50fc5947
AraQuispe <40554360+AraQuispe@users.noreply.github.com>
2021-10-02 08:06:56 -0500

    Initial commit

---
+++ app/src/main/java/com/lab02/visitamedica/Registrar_Visita_Activity.java
@@ -0,0 +1,13 @@
+package com.lab02.visitamedica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registrar_Visita_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_visita);
+    }
+}
