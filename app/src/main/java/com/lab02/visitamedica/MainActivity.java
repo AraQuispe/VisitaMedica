b896f7fb5f709cfd3129c3760700212a7675018b
CarlosMestas <42045414+CarlosMestas@users.noreply.github.com>
2021-10-02 10:39:32 -0500

    Enviar correo, Intent implicito

--- app/src/main/java/com/lab02/visitamedica/MainActivity.java
+++ app/src/main/java/com/lab02/visitamedica/MainActivity.java
@@ -1,13 +1,37 @@
 package com.lab02.visitamedica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSendEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSendEmail = (Button) findViewById(R.id.btnEnviarCorreo);
+
+        buttonSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String destino  = "";
+                String asunto   = "";
+                String mensaje  = "";
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{destino});
+                intent.putExtra(Intent.EXTRA_SUBJECT, asunto);
+                intent.putExtra(Intent.EXTRA_TEXT, mensaje);
+                intent.setType("message/rfc822");
+                startActivity(Intent.createChooser(intent,"Choose Mail App"));
+            }
+        });
+
     }
 }
