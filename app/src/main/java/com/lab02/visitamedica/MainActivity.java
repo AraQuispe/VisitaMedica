04bef84891bf0803babb6df679edd8f3874c07ef
AnaLu72651718 <aaangles@unsa.edu.pe>
2021-10-05 09:19:57 -0500

    Datos Paciente

--- app/src/main/java/com/lab02/visitamedica/MainActivity.java
+++ app/src/main/java/com/lab02/visitamedica/MainActivity.java
@@ -1,14 +1,19 @@
 package com.lab02.visitamedica;
 
+import static com.lab02.visitamedica.R.id.txtDatoPac;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonSendEmail;
+    private Button buttonSendEmail, btnregistrar;
+    private TextView mostrarDatosPacientes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +34,34 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra(Intent.EXTRA_SUBJECT, asunto);
                 intent.putExtra(Intent.EXTRA_TEXT, mensaje);
                 intent.setType("message/rfc822");
+
                 startActivity(Intent.createChooser(intent,"Elija una aplicación de correo"));
+
             }
         });
 
+        mostrarDatosPacientes   = (TextView)findViewById (R.id.txtDatoPac);
+        btnregistrar       = (Button) findViewById(R.id.btnRegistrarPac2);
+
+        String nombres   = getIntent().getStringExtra("nombres");
+        String apellidos    = getIntent().getStringExtra("apellidos");
+        String dni          = getIntent().getStringExtra("dni");
+        String direccion    = getIntent().getStringExtra("direccion");
+        String correo       = getIntent().getStringExtra("correo");
+
+        mostrarDatosPacientes.setText("Nombres: "+nombres+"\nApellidos: "+apellidos+"\nDNI: "+dni+"\nDirección: "+direccion+"\nCorreo: "+correo);
+
+
+        btnregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i    = new Intent(MainActivity.this,Registrar_Paciente_Activity.class);
+
+                startActivity(i);
+            }
+        });
+
+
     }
+
 }
--- app/src/main/java/com/lab02/visitamedica/Registrar_Paciente_Activity.java
+++ app/src/main/java/com/lab02/visitamedica/Registrar_Paciente_Activity.java
@@ -1,13 +1,65 @@
 package com.lab02.visitamedica;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Registrar_Paciente_Activity extends AppCompatActivity {
+
+    EditText nombres,apellidos,dni,direccion,correo;
+    Button registrar,cancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_paciente);
+
+        nombres     = findViewById(R.id.editPersonName);
+        apellidos   = findViewById(R.id.editPersonLastName);
+        dni         = findViewById(R.id.editDNI);
+        direccion   = findViewById(R.id.editPersonAddress);
+        correo      = findViewById(R.id.editEmailAddress);
+
+        registrar   = findViewById(R.id.btnRegister);
+        cancelar    = findViewById(R.id.btnCancel);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Registrar_Paciente_Activity.this,MainActivity.class);
+
+                i.putExtra("nombres",nombres.getText().toString());
+                i.putExtra("apellidos",apellidos.getText().toString());
+                i.putExtra("dni",dni.getText().toString());
+                i.putExtra("direccion",direccion.getText().toString());
+                i.putExtra("correo",correo.getText().toString());
+
+                startActivity(i);
+
+            }
+        });
+
+        cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                limpiarDatos();
+            }
+        });
     }
+
+    private void limpiarDatos(){
+        nombres.setText("");
+        apellidos.setText("");
+        dni.setText("");
+        direccion.setText("");
+        correo.setText("");
+    }
+
 }
+
+
